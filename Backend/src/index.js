67e29a0a1e94327456ef5952172841ce077e068d
Ariel Macario <38597009+Macario12@users.noreply.github.com>
2022-10-16 00:51:25 -0600

    Revert "Se creo rama ProyectoF1"

--- Backend/src/index.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app');
-
-//Settings
-const PORT = process.env.PORT || 3000;
-
-//Starting the server
-app.listen( PORT, () =>{
-    console.log(`server listening on ${PORT}`);
-});
--- Backend/src/routes/integrantes.route.js
+++
@@ -1,9 +0,0 @@
-var controller = require('../controllers/integrantes.controller');
-var express = require('express')
-let api = express.Router()
-
-api.post('/add', controller.addIntegrante);
-api.get('/getAll', controller.getIntegrantes);
-api.post('/get', controller.getIntegrante);
-
-module.exports = api;
